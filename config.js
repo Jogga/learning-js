79710d6e8ba907b1cc8d9bcdbf55ea373aab6ee9
Jogga <joachim@froestl.com>
2015-01-14 21:32:26 +0100

    adds config and example post

---
+++ config.js
@@ -0,0 +1,14 @@
+/*
+ * Config File
+ */
+
+module.exports = {
+	publicDir: 'public',
+	postsDir: '_posts',
+	postBase: 'posts',
+	layoutsDir: '_layouts',
+	includesDir: '_includes',
+	defaultLayout: 'default',
+	indexFile: 'index',
+	title: 'quest.js - my journey to JS'
+};
